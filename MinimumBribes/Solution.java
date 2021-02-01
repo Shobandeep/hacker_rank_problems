a126433612aa4fd9cb6ff9ff9bdb013be6f510f7
funk <shobandeep@gmail.com>
2021-02-01 01:33:21 -0500

    initial commit

---
+++ MinimumBribes/Solution.java
@@ -0,0 +1,67 @@
+package MinimumBribes;
+
+public class Solution {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        int bribeCount = 0;
+
+        for(int i = 0; i < q.length; i++) {
+            int deltaPosition = q[i] - (i+1);
+            if( deltaPosition > 2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+            for(int j = Math.max(0, q[i]-2); j < i; j++) {
+                // if someone with a higher sticker number is front, this person has taken a bribe
+                if(q[j] > q[i])
+                    bribeCount++;
+            }
+        }
+
+
+        System.out.println(bribeCount);
+    }
+
+    static void minimumBribesBackwards(int[] q) {
+        int bribeCount = 0;
+
+        for(int j = q.length; j > 0; j--) {
+            int index = 0;
+            // find the index of this sticker
+            while(j != q[index]) index++;
+
+            // if the sticker is >2 away from it's position, there were too many bribes
+            int deltaPosition = j - (index+1);
+            if( deltaPosition > 2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+            // do nothing if they haven't moved
+            else if(deltaPosition == 0)
+                continue;
+
+            // reverse the bribes until this person is back in
+            // their original position
+            while(q[j-1] != j) {
+                q[index] = q[index+1];
+                q[index+1] = j;
+                index++;
+                bribeCount++;
+            }
+        }
+
+        System.out.println(bribeCount);
+    }
+
+    public static void main(String[] args) {
+
+        int[] q = {2, 1, 5, 3, 4};
+        // int[] q = {1, 2, 5, 3, 7, 8, 6, 4};
+        // int[] q = {2, 5, 1, 3, 4};
+
+        minimumBribes(q);
+    }
+}
+
+
---
+++ MinimumSwaps/Solution.java
@@ -0,0 +1,42 @@
+package MinimumSwaps;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int swaps = 0;
+
+        int temp;
+        int limit = (arr.length / 2 == 0) ? arr.length / 2 : (arr.length / 2)+1;
+        while(limit != 0) {
+            for(int i = 0; i < arr.length; i++) {
+                if(arr[i] != i+1) {
+                    temp = arr[i];
+                    arr[i] = arr[temp-1];
+                    arr[temp-1] = temp;
+                    swaps++;
+                }
+            }
+            limit = limit / 2;
+        }
+
+
+//        for(int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i] + " ");
+//        }
+//        System.out.println();
+//
+//        System.out.println(swaps);
+
+        return swaps;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 3, 5, 2, 4, 6, 7};
+
+        minimumSwaps(arr);
+    }
+}
+
+
